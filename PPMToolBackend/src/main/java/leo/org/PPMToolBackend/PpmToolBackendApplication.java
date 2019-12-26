4fc33668c3224b690fc346f2cfcfb903c12470b8
LIAOERDONG <ledbupt@163.com>
2019-12-26 14:15:50 -0600

    first commit

---
+++ PPMToolBackend/src/main/java/leo/org/PPMToolBackend/PpmToolBackendApplication.java
@@ -0,0 +1,13 @@
+package leo.org.PPMToolBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmToolBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpmToolBackendApplication.class, args);
+	}
+
+}
---
+++ PPMToolBackend/src/test/java/leo/org/PPMToolBackend/PpmToolBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package leo.org.PPMToolBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmToolBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
